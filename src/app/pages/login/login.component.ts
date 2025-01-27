7665af9e8bcc0bc747be308faa3ae3f321f96fc4
Gustavo Rojas Valdivia <doc.gustavo.rojas.va@unifranz.edu.bo>
2025-01-27 03:37:42 -0400

    Implemented login functionality

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  credentials = { email: '', password: '' };
+  errorMessage = '';
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login() {
+    this.authService.login(this.credentials).subscribe({
+      next: response => {
+        localStorage.setItem('token', response.token);
+        this.router.navigate(['/dashboard']);
+      },
+      error: () => {
+        this.errorMessage = 'Credenciales incorrectas';
+      }
+    });
+  }
 }
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  constructor(private authService: AuthService, private router: Router) {}
 
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
